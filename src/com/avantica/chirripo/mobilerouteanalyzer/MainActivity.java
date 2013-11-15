d447d9d265d3e0471e262ac234ebb40eb5d567f8
bryancr89 <bazofeifa89@gmail.com>
2013-11-15 13:46:51 -0600

    Added the tab swipe.

--- src/com/avantica/chirripo/mobilerouteanalyzer/MainActivity.java
+++ src/com/avantica/chirripo/mobilerouteanalyzer/MainActivity.java
@@ -1,24 +1,79 @@
 package com.avantica.chirripo.mobilerouteanalyzer;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+import com.avantica.chirripo.tabs.TabSwipe;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends FragmentActivity implements
+	ActionBar.TabListener {
+		 
+	private ViewPager viewPager;
+	private TabSwipe mAdapter;
+	private ActionBar actionBar;
+	// Tab titles
+	private String[] tabs = { "My routes", "Add Route"};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabSwipe(getSupportFragmentManager());
+		
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
+		
+		// Adding Tabs
+		for (String tab_name : tabs) {
+		    actionBar.addTab(actionBar.newTab().setText(tab_name)
+		            .setTabListener(this));
+		}
+		
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			 
+		    @Override
+		    public void onPageSelected(int position) {
+		        // on changing the page
+		        // make respected tab selected
+		        actionBar.setSelectedNavigationItem(position);
+		    }
+		 
+		    @Override
+		    public void onPageScrolled(int arg0, float arg1, int arg2) {
+		    }
+		 
+		    @Override
+		    public void onPageScrollStateChanged(int arg0) {
+		    }
+		});
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+        viewPager.setCurrentItem(tab.getPosition());
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
---
+++ src/com/avantica/chirripo/tabs/TabSwipe.java
@@ -0,0 +1,33 @@
+package com.avantica.chirripo.tabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabSwipe extends FragmentPagerAdapter {
+
+    public TabSwipe(FragmentManager fm) {
+        super(fm);
+    }
+ 
+    @Override
+    public Fragment getItem(int index) {
+ 
+        switch (index) {
+        case 0:
+            // Top Rated fragment activity
+            return new MyRoutes();
+        case 1:
+            // Games fragment activity
+            return new Route();
+        }
+ 
+        return null;
+    }
+ 
+    @Override
+    public int getCount() {
+        return 2;
+    }
+ 
+}
