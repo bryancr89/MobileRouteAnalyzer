79999ce1ab9f1ef11684031a5f644543e64376a3
juankcr27 <juank.arce@hotmail.com>
2013-11-19 13:23:20 -0600

    getting waypoints

--- src/net/chirripo/tabs/Route.java
+++ src/net/chirripo/tabs/Route.java
@@ -1,11 +1,14 @@
 package net.chirripo.tabs;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.chirripo.mobilerouteanalyzer.R;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +26,9 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Route extends Fragment {
@@ -36,33 +41,71 @@ public class Route extends Fragment {
 	private View rootView;
 	private TextView speedTextView;
 	private ImageButton startButton;
- 
+	private ImageButton stopButton;
+	private Marker startMarker;
+	private Marker endMarker;
+	private List<LatLng> wayPointsList = new ArrayList();
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         rootView = inflater.inflate(R.layout.route, container, false);
         speedTextView = (TextView) rootView.findViewById(R.id.show_speed);
-        startButton = (ImageButton)rootView.findViewById(R.id.start_button);
+       
         
-        //onClick listener for start button
+        //initialization and onClick listener for start button
+        startButton = (ImageButton)rootView.findViewById(R.id.start_button);
         startButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				map.addMarker(new MarkerOptions()
+				
+				if(startMarker != null){
+					startMarker.remove();
+				}
+				
+				startMarker = map.addMarker(new MarkerOptions()
 		    	.position(myLocation)
-		    	.title("Start Point"));
+		    	.title("Start Point")
+		    	.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+				
+				//insert first way point to the list
+				wayPointsList.add(myLocation);
 			}
 		});
-         
+        
+        //initialization and onClick listener for end button
+        stopButton = (ImageButton)rootView.findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				if(endMarker != null){
+					endMarker.remove();
+				}
+				
+				endMarker = map.addMarker(new MarkerOptions()
+		    	.position(myLocation)
+		    	.title("End Point")
+		    	.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+				
+				//insert last way point to the list
+				wayPointsList.add(myLocation);
+			}
+		});
+        
+        
+        //map initialization 
         map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();        
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         map.setMyLocationEnabled(true);
                
+        //call the function to relocated the user in its current position
         centerMapOnMyLocation();
-        
+             
         return rootView;
     }
 
@@ -82,8 +125,8 @@ public class Route extends Fragment {
         @Override
         public void onConnected(Bundle arg0) {
             LocationRequest locationRequest = LocationRequest.create();
-            locationRequest.setFastestInterval(0);
-            locationRequest.setInterval(0).setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setFastestInterval(1000);
+            locationRequest.setInterval(1000).setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
             locationClient.requestLocationUpdates(locationRequest, mLocationListener);            
             
             location = locationClient.getLastLocation();
@@ -119,7 +162,13 @@ public class Route extends Fragment {
                 String samplingRate = (new DecimalFormat("0.0000").format(1/delayBtnEvents));     
 
                 float speed = (float) (location.getSpeed() * 3.6);  // Converting m/s to Km/hr
-                speedTextView.setText(speed + " kmph" + ", " + samplingRate + " Hz"); //Updating UI        	
+                speedTextView.setText(speed + " kmph" + ", " + samplingRate + " Hz"); //Updating UI 
+                
+                //insert way points to the list while moving on the road 
+                if(!wayPointsList.contains(myLocation) && !wayPointsList.isEmpty()){
+                	wayPointsList.add(myLocation);
+                }
+				
         }
     };   
 }
