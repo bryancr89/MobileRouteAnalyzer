cc6c2289b812678a03db07b21a67dee024a6772e
bryancr89 <bazofeifa89@gmail.com>
2013-12-04 09:26:57 -0600

    Shown the map

--- src/net/chirripo/tabs/RunningRoute.java
+++ src/net/chirripo/tabs/RunningRoute.java
@@ -1,19 +1,18 @@
 package net.chirripo.tabs;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 
 import net.chirripo.logic.Logic;
 import net.chirripo.mobilerouteanalyzer.R;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 
-public class RunningRoute extends Fragment {	
+public class RunningRoute extends Activity {	
 	
 	private Logic _logic;
 	View rootView;
@@ -21,22 +20,26 @@ public class RunningRoute extends Fragment {
 	private GoogleMap _map;
 	
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.running_route, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.running_route);
+        setTitle("My new title");
+        _map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map22)).getMap();
+        /*rootView = findViewById(R.layout.running_route);
         ctx = rootView.getContext();
         _logic = new Logic(ctx);
-        
+        MapView mapView = (MapView) findViewById(R.id.mapviewtest);
+
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
         //map initialization 
-        _map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.running_map))
-                .getMap();        
+        _map = mapView.getMap();    
         _map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        _map.setMyLocationEnabled(true);
+        _map.setMyLocationEnabled(true);*/
         
         //Bundle bundle = getIntent().getExtras();
         //get parameters from the other activity
         
-        return rootView;
     }
     
 }
